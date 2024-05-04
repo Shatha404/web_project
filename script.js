b377aeb3195e737cd3cb8e08971e23c5568e0851
shahadfal <shahad3345@gmail.com>
2024-05-04 15:27:58 +0300

    cafes &clubs
    designed cafes and restaurant, clubs sections

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ var typed = new Typed (".text" ,{
         backDelay:1000,
         loop:true
 });
+
+const cafesContainer = [...document.querySelectorAll('.cafes-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+cafesContainer.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    });
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    });
+});
+
