2cc700eb793b1dd9ab70d0a9046cc6f958db292e
Shatha404 <105718858+Shatha404@users.noreply.github.com>
2024-05-23 18:51:05 +0300

    edit index.js ,connect to mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-const express = require('express')
-const { request } = require('http')
+// app.js
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const path = require('path'); // Add path module
+
+// Database and Passport setup
+require('./config/database');
+require('./config/passport-setup');
+
+// EJS template engine setup
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // Ensure this line is present
+
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(session({
+    secret: 'lorem ipsum',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 * 15 }
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', (req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
-const app = express()
+//render universty pages
+app.get('/imamu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'imamu.html'));
+});
+app.get('/ksu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'KSU.html'));
+});
+app.get('/pnu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pnu.html'));
+});
 
-app.get('/',(request,response)=>{
-    response.send('hello')
-})
 
 
-app.listen(3000, ()=> console.log('Server started to work on port 3000'))
+
+// Routes setup
+const events = require('./routes/event-routes');
+const users = require('./routes/user-routes');
+app.use('/Home', events);
+app.use('/users', users);
+
+// Listen on port 3000
+app.listen(3000, () => {
+    console.log('App is working on port 3000');
+});
